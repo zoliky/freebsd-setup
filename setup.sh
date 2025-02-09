c8d8592092931e98c1e8c70d1951d68dc54f740e
Zoltán Király <3882981+zoliky@users.noreply.github.com>
2025-02-09 20:40:25 +0200

    Sync

--- setup.sh
+++ setup.sh
@@ -80,8 +80,8 @@ fi
 # Install utilities and fonts
 install_packages \
   mpv vim fzf dfc zip htop wget kitty rclone rsync meson lsblk \
-  yt-dlp mixertui hack-font fastfetch portmaster poudriere \
-  zsh ohmyzsh
+  yt-dlp mixertui hack-font webfonts fastfetch portmaster \
+  poudriere zsh ohmyzsh
 
 # Install Firefox
 install_packages firefox
